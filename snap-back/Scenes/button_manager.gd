24e5b4a2575f3f85fab635fd2e0b2f60df54303c
dmh4054 <dmh4054@rit.edu>
2026-09-24 02:14:54 -0400

    Yall I tried

--- snap-back/Scenes/button_manager.gd
+++ snap-back/Scenes/button_manager.gd
@@ -1,17 +1,34 @@
 extends Control
 
+# Character Data Structure
+class CharacterData:
+	var name: String
+	var role: String
+	var texture: Texture2D
 
-# Called when the node enters the scene tree for the first time.
+	func _init(p_name: String, p_role: String, p_texture: Texture2D) -> void:
+		name = p_name
+		texture = p_texture
+
+@onready var avatar_texture: TextureRect = $CardPanel/VBoxContainer/CarouselHBox/DisplayVBox/AvatarTexture
+@onready var name_label: Label = $CardPanel/VBoxContainer/CarouselHBox/DisplayVBox/NameLabel
+@onready var prev_button: Button = $CardPanel/VBoxContainer/CarouselHBox/PrevButton
+@onready var next_button: Button = $CardPanel/VBoxContainer/CarouselHBox/NextButton
+@onready var lock_button: Button = $CardPanel/VBoxContainer/LockInButton
+@onready var status_label: Label = $CardPanel/VBoxContainer/StatusLabel
+
+signal character_selected(character_data: CharacterData)
+
+var characters: Array[CharacterData] = []
+var current_index: int = 0
+var is_locked: bool = false
+
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-	
-
-
-func _on_button_pressed() -> void:
-	pass # Replace with function body.
+	# Load sample character data
+	characters = [
+		CharacterData.new("Valerie", "Vanguard / Tank", preload("res://icon.svg")),
+		CharacterData.new("Jax", "Duelist / Speed", preload("res://icon.svg")),
+		CharacterData.new("Aura", "Support / Healer", preload("res://icon.svg")),
+		CharacterData.new("Kael", "Mage / Crowd Control", preload("res://icon.svg"))
+	]
